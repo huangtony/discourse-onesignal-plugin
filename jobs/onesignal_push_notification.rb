95b7b890d98e8632a8af0bccd1b1cd7defea4536
Tony Huang <huang.tony.88@gmail.com>
2019-10-12 21:59:00 +1000

    Add custom notification text

--- jobs/onesignal_push_notification.rb
+++ jobs/onesignal_push_notification.rb
@@ -1,10 +1,49 @@
 module ::Jobs
   class OnesignalPushNotification < Jobs::Base
 
-    ONE_SIGNAL_API = 'https://onesignal.com/api/v1/notifications'.freeze
+    ONE_SIGNAL_API = "https://onesignal.com/api/v1/notifications".freeze
 
     def execute(args)
+
+      # {
+      #     "notification_type"=>6,
+      #     "post_number"=>4,
+      #     "topic_title"=>"Topic title",
+      #     "topic_id"=>18,
+      #     "excerpt"=>"excerpt",
+      #     "username"=>"admin",
+      #     "post_url"=>"/t/topic-title"
+      # }
+      #
+
       payload = args["payload"]
+      receiver = args[:username]
+      sender = payload[:username]
+      topic_title = payload[:topic_title]
+      excerpt = payload[:excerpt]
+      notification_type = payload[:notification_type]
+      post_url = payload[:post_url]
+
+      case notification_type
+      when Notification.types[:mentioned]
+        heading = "#{sender} mentioned you - #{topic_title}"
+        contents = excerpt
+      when Notification.types[:replied]
+        heading = "Reply from #{sender} - #{topic_title}"
+        contents = excerpt
+      when Notification.types[:private_message]
+        heading = "Message from #{sender} - #{topic_title}"
+        contents = excerpt
+      when Notification.types[:posted]
+        heading = "#{sender} posted in #{topic_title}"
+        contents = excerpt
+      when Notification.types[:linked]
+        heading = "#{sender} linked to your post - #{topic_title}"
+        contents = excerpt
+      else
+        heading = topic_title
+        contents = "#{sender}: #{excerpt}"
+      end
 
       filters = [
           {"field": "tag", "key": "username", "relation": "=", "value": args["username"]}
@@ -12,9 +51,9 @@ module ::Jobs
 
       params = {
           "app_id" => SiteSetting.onesignal_app_id,
-          "contents" => {"en" => "#{payload[:username]}: #{payload[:excerpt]}"},
-          "headings" => {"en" => payload[:topic_title]},
-          "data" => {"discourse_url" => payload[:post_url]},
+          "contents" => {"en" => contents},
+          "headings" => {"en" => heading},
+          "data" => {"discourse_url" => post_url},
           "ios_badgeType" => "Increase",
           "ios_badgeCount" => "1",
           "filters" => filters
@@ -22,17 +61,17 @@ module ::Jobs
 
       uri = URI.parse(ONE_SIGNAL_API)
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if uri.scheme == 'https'
+      http.use_ssl = true if uri.scheme == "https"
 
       request = Net::HTTP::Post.new(uri.path,
-                                    'Content-Type'  => 'application/json;charset=utf-8',
-                                    'Authorization' => "Basic #{SiteSetting.onesignal_rest_api_key}")
+                                    "Content-Type"  => "application/json;charset=utf-8",
+                                    "Authorization" => "Basic #{SiteSetting.onesignal_rest_api_key}")
       request.body = params.as_json.to_json
       response = http.request(request)
 
       case response
       when Net::HTTPSuccess then
-        Rails.logger.info("Push notification sent via OneSignal to #{args['username']}.")
+        Rails.logger.info("Push notification sent via OneSignal to #{receiver}.")
       else
         Rails.logger.error("OneSignal error")
         Rails.logger.error("#{request.to_yaml}")
--- plugin.rb
+++ plugin.rb
@@ -7,16 +7,16 @@
 after_initialize do
   if SiteSetting.onesignal_push_enabled
 
-    load File.expand_path('jobs/onesignal_push_notification.rb', __dir__)
+    load File.expand_path("jobs/onesignal_push_notification.rb", __dir__)
 
     DiscourseEvent.on(:post_notification_alert) do |user, payload|
 
       if SiteSetting.onesignal_app_id.nil? || SiteSetting.onesignal_app_id.empty?
-          Rails.logger.warn('OneSignal App ID is missing')
+          Rails.logger.warn("OneSignal App ID is missing")
           return
       end
       if SiteSetting.onesignal_rest_api_key.nil? || SiteSetting.onesignal_rest_api_key.empty?
-          Rails.logger.warn('OneSignal REST API Key is missing')
+          Rails.logger.warn("OneSignal REST API Key is missing")
           return
       end
 
